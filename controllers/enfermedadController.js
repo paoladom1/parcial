36b8dbc14166aaee78634ee82bb015ceff8cdb34
Paola Dominguez <paoladomdiaz@gmail.com>
2019-11-23 16:44:13 -0600

    controlador

--- controllers/enfermedadController.js
+++ controllers/enfermedadController.js
@@ -16,7 +16,7 @@ module.exports.getOne = (req, res, next) => {
 };
 
 module.exports.getAll = (req, res, next) => {
-    var perPage = Number(req.query.size) || 5,
+    var perPage = Number(req.query.size) || 10,
         page = req.query.page > 0 ? req.query.page : 0;
 
     var sortProperty = req.query.sortby || "createdAt",
